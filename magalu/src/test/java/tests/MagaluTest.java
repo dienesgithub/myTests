79817f761c56656bc558a178ead12a6dccf3d06e
Dienes Stein <38922688+dienes-stein@users.noreply.github.com>
2020-07-05 09:56:56 -0300

    Ajustes nos testes
    Validacao no produto do carrinho

--- magalu/src/test/java/tests/MagaluTest.java
+++ magalu/src/test/java/tests/MagaluTest.java
@@ -33,7 +33,6 @@ public class MagaluTest {
 		assertTrue("Titulo esperado: '" + titulo + "' diferente do obtido: '" + driver.getTitle() + "'", driver.getTitle().contentEquals(titulo));
 		Thread.sleep(3000);
 		
-		
 		WebElement buscaTexto = driver.findElement(By.xpath("//*[@id='inpHeaderSearch']"));
 		buscaTexto.sendKeys("controle elite xbox one");
 		Thread.sleep(3000);
@@ -48,12 +47,9 @@ public class MagaluTest {
 		WebElement addSacolaBotao = driver.findElement(By.xpath("/html/body/div[3]/div[5]/div[1]/div[4]/div[2]/button"));
 		addSacolaBotao.click();
 		Thread.sleep(10000);
-		WebElement continuarComprandoBotao = driver.findElement(By.xpath("//*[@id='root']/div/div[2]/div/div[2]/div/div[4]/div[2]/div[1]/a"));
-		continuarComprandoBotao.click();
-		Thread.sleep(10000);
-		WebElement qtdSacola = driver.findElement(By.xpath("/html/body/div[3]/div[2]/div[1]/div[1]/header/div/div[1]/div/div/div[2]/div[1]/a[1]/div/i"));
-		assertTrue("Espera-se apenas 1 item na sacola", qtdSacola.getText().contentEquals("1"));
-		Thread.sleep(90000);
+		WebElement produtoSacola = driver.findElement(By.xpath("//*[@id='root']/div/div[2]/div/div[2]/div/div[2]/div[2]/div/div/div[1]/div/a/p[1]"));
+		assertTrue("O produto esperado na sacola: " + primeiroProduto + " é diferente do obtido: " + produtoSacola.getText(), produtoSacola.getText().contentEquals(primeiroProduto));	
+	
 	}
-
+	
 }
